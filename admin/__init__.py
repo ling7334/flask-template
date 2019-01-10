08c4ae11d1a211183b5d59ef7edcb4707c55fda8
ling7334 <1845797+ling7334@users.noreply.github.com>
2019-01-10 10:41:36 +0800

    rearrange files

--- admin/__init__.py
+++ admin/__init__.py
@@ -2,5 +2,5 @@ from flask import Blueprint
 
 adminBlueprint = Blueprint('admin', __name__)
 
-from .view import UserAPI
+from .views import UserAPI
 adminBlueprint.add_url_rule('/user', view_func=UserAPI.as_view('users'))
